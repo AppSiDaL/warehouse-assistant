e0272b2c18ecb59d4cf13f5bbeca34fa5085d393
AppSiDaL <gilielcrack343@gmail.com>
2024-08-31 23:21:39 +0000

    ✨ Add camera functionality to open two cameras

--- server/main.py
+++ server/main.py
@@ -1,15 +1,17 @@
-from typing import Union
-
+import threading
+import uvicorn
 from fastapi import FastAPI
+from utils.video import capture_frames
+from controllers.main import router
 
 app = FastAPI()
+app.include_router(router)
 
+if __name__ == '__main__':
+    # Inicia el hilo de captura de cuadros
+    t = threading.Thread(target=capture_frames)
+    t.daemon = True
+    t.start()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # Inicia el servidor FastAPI
+    uvicorn.run(app, host='0.0.0.0', port=8000)
