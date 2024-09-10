5167764aa1232082b51b3e7b0fb02d8aed66488d
AppSiDaL <gilielcrack343@gmail.com>
2024-09-10 08:14:29 -0600

    Refactor control.py to handle serial connection errors

--- server/utils/control.py
+++ server/utils/control.py
@@ -1,6 +1,8 @@
 import serial
-
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Ajusta el puerto y la velocidad según sea necesario
+try:
+    ser = serial.Serial('/dev/ttyACM0', 9600)  # Ajusta el puerto y la velocidad según sea necesario
+except:
+    print("No se pudo conectar al puerto /dev/ttyACM0")
 
 def send_command(command):
     if command in ['forward', 'backward', 'left', 'right', 'stop']:
