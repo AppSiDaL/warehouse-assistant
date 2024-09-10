58ece35e701a521c0603116676e5e90b3f8a293a
AppSiDaL <gilielcrack343@gmail.com>
2024-09-10 08:13:49 -0600

    Add video source, model path, and output JSON configuration to cv/config.py

---
+++ server/cv/config.py
@@ -0,0 +1,3 @@
+VIDEO_SOURCE = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/test1.mp4"
+MODEL_PATH = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/best.pt"
+OUTPUT_JSON = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/plataform_data.json"
