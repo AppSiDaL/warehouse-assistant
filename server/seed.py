f2a03bd4961eb576a24ad092b50e0a07151d8b7d
AppSiDaL <gilielcrack343@gmail.com>
2024-09-12 20:59:31 -0600

    Refactor seed.py: Update database seeding logic

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, text
 from models.items import Item
+from models.platforms import Platform
 from utils.db import SessionLocal, engine, Base
 import json
 
@@ -7,9 +9,12 @@ Base.metadata.create_all(bind=engine)
 
 db_session = SessionLocal()
 
+# Truncate the items table for SQLite
+db_session.execute(text("DELETE FROM items;"))
+db_session.commit()
+
 with open("seeders/items.json") as f:
     items_data = json.load(f)
-    db_session = SessionLocal()
     items = [
         Item(
             code=item["code"],
@@ -17,7 +22,6 @@ with open("seeders/items.json") as f:
             isBox=item["isBox"],
             description=item["description"],
             dimension=item["dimension"],
-            quantity=item["quantity"],
             price=item["price"]
         )
         for item in items_data
@@ -25,4 +29,16 @@ with open("seeders/items.json") as f:
     db_session.bulk_save_objects(items)
     db_session.commit()
 
+with open("seeders/platforms.json") as f:
+    platforms_data = json.load(f)
+    platforms = [
+        Platform(
+            code=platform["code"],
+            items_count=platform["items_count"],
+            dimension=platform["dimension"]
+        )
+        for platform in platforms_data
+    ]
+    db_session.bulk_save_objects(platforms)
+    db_session.commit()
 db_session.close()
