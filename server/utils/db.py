000fff72be5f5bdc109c2afa6dcd0dc99d2162bf
AppSiDaL <gilielcrack343@gmail.com>
2024-09-12 21:00:28 -0600

    Refactor db.py: Add foreign key pragma on connect

--- server/utils/db.py
+++ server/utils/db.py
@@ -1,9 +1,12 @@
-from sqlalchemy import create_engine,StaticPool
-from sqlalchemy.orm import sessionmaker,declarative_base
-from .config import DB_URL,ENV
+from sqlalchemy import create_engine, StaticPool, event
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import DB_URL, ENV
+
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    dbapi_con.execute('pragma foreign_keys=ON')
 
 try:
-    if ENV=="testing":
+    if ENV == "testing":
         engine = create_engine(
             DB_URL,
             connect_args={
@@ -11,10 +14,12 @@ try:
             },
             poolclass=StaticPool,
         )
+        event.listen(engine, 'connect', _fk_pragma_on_connect)
         SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-    engine = create_engine(DB_URL, echo=True)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    else:
+        engine = create_engine(DB_URL, echo=True)
+        event.listen(engine, 'connect', _fk_pragma_on_connect)
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 except Exception as e:
     print("Error: ", e)
