7c47e6e7f0b0b44144afc048728ceea45bbacd8f
AppSiDaL <gilielcrack343@gmail.com>
2024-09-12 20:59:45 -0600

    Update video source path in config.py

--- server/cv/config.py
+++ server/cv/config.py
@@ -1,3 +1,3 @@
-VIDEO_SOURCE = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/test1.mp4"
+VIDEO_SOURCE = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/video/qr-large-p1.mp4"
 MODEL_PATH = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/best.pt"
 OUTPUT_JSON = "/Users/gilbertodavalosnava/Documents/AppSiDaL/warehouse-assistant/server/plataform_data.json"
