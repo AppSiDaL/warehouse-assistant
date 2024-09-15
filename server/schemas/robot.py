40186046495fed73550106c32a86adf5e49998b9
AppSiDaL <gilielcrack343@gmail.com>
2024-09-14 20:51:03 -0600

    Refactor robot request schema to include optional fields

--- server/schemas/robot.py
+++ server/schemas/robot.py
@@ -17,3 +17,16 @@ class RobotResponseSchema(BaseModel):
         
 class RobotRequestSchema(RobotResponseSchema):
     pass
+
+class OptionalRobotRequestSchema(RobotResponseSchema):
+    id: Optional[int] = Field(None, description="Id of the robot")
+    microcontroller: Optional[str] = Field(None, description="Microcontroller of the robot")
+    architecture: Optional[str] = Field(None, description="Architecture of the robot")
+    cpu_usage: Optional[int] = Field(None, description="CPU usage of the robot")
+    ram_usage: Optional[int] = Field(None, description="RAM usage of the robot")
+    storage_usage: Optional[int] = Field(None, description="Storage usage of the robot")
+    location: Optional[str] = Field(None, description="Location of the robot")
+    
+    class Config:
+        from_attributes = True
+        orm_mode = True
