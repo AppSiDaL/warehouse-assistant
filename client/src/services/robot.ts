f27bdc20d49ccd404b2cc9c3285c9094d6b198a6
AppSiDaL <gilielcrack343@gmail.com>
2024-09-14 20:50:13 -0600

    Refactor robot service and add fetchRobotPosition and fetchMap functions

---
+++ client/src/services/robot.ts
@@ -0,0 +1,37 @@
+import { SERVER_URL } from "../constants";
+const URL = `${SERVER_URL}/robot/control`;
+
+const sendCommand = async (command: string): Promise<any> => {
+  const response = await fetch(`${URL}/${command}`, {
+    method: "POST",
+  });
+  if (!response.ok) {
+    throw new Error(`Error fetching items: ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+const fetchMap = async () => {
+  const response = await fetch(`${SERVER_URL}/robot/get_map_image`);
+  if (!response.ok) {
+    throw new Error(`Error fetching items: ${response.statusText}`);
+  }
+  const blob = await response.blob();
+
+  return blob;
+};
+
+const fetchRobotPosition =async () =>{
+  const response = await fetch(`${SERVER_URL}/robot/get_location`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch robot position");
+  }
+  return response;
+}
+
+export default {
+  sendCommand,
+  fetchRobotPosition,
+  fetchMap,
+};
