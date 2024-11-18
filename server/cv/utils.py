c4259185546a80cacdd48e8aa6cf7da00d418aa9
AppSiDaL <gilielcrack343@gmail.com>
2024-11-18 13:53:00 -0600

    Refactor distance estimation and text drawing functions in utils.py for improved readability and consistency

--- server/cv/utils.py
+++ server/cv/utils.py
@@ -7,49 +7,24 @@ from services.robot import update_robot
 # Known parameters
 FOCAL_LENGTH = 800  # Example: focal length of the camera in pixels
 
-# URL of the endpoint to get item description
-
-
 def estimate_distance(known_width, focal_length, pixel_width):
     return (known_width * focal_length) / pixel_width
-
 
 def draw_polygon(frame, polygon):
     cv2.polylines(frame, [polygon], isClosed=True, color=(0, 255, 0), thickness=2)
 
-
 def get_sizes(platform_code, item_code):
     platform = get_platform(platform_code)
     item = get_item(item_code)
-    know_widht = item["dimension"].split("x")[0]
+    known_width = item["dimension"].split("x")[0]
     box_depth = item["dimension"].split("x")[1]
     platform_depth = platform["dimension"].split("x")[1]
-    return float(know_widht)/100, float(box_depth)/100, float(platform_depth)/100
+    return float(known_width)/100, float(box_depth)/100, float(platform_depth)/100
 
-
-def draw_text(
-    frame, text, position, font_scale=0.5, color=(255, 255, 255), thickness=2
-):
-    (text_width, text_height), baseline = cv2.getTextSize(
-        text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness
-    )
-    cv2.rectangle(
-        frame,
-        (position[0], position[1] - text_height - 10),
-        (position[0] + text_width, position[1]),
-        (0, 0, 0),
-        -1,
-    )
-    cv2.putText(
-        frame,
-        text,
-        (position[0], position[1] - 5),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        font_scale,
-        color,
-        thickness,
-    )
-
+def draw_text(frame, text, position, font_scale=0.5, color=(255, 255, 255), thickness=2):
+    (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness)
+    cv2.rectangle(frame, (position[0], position[1] - text_height - 10), (position[0] + text_width, position[1]), (0, 0, 0), -1)
+    cv2.putText(frame, text, (position[0], position[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness)
 
 def estimate_box_count(distances, average_box_depth, pallet_depth, current_count):
     if not distances:
@@ -58,11 +33,9 @@ def estimate_box_count(distances, average_box_depth, pallet_depth, current_count
     num_boxes_in_depth = int(pallet_depth / average_box_depth)
     return num_boxes_in_depth * current_count
 
-
 def save_data_to_json(plataform_data, output_json):
     with open(output_json, "w") as f:
         json.dump(plataform_data, f, indent=4)
-
 
 def process_detections(
     frame,
@@ -92,45 +65,25 @@ def process_detections(
             type = box_code
         else:
             platform_id_text = ""
-        know_widht, box_depth, platform_depth = get_sizes(platform_id_text, box_code)
+        known_width, box_depth, platform_depth = get_sizes(platform_id_text, box_code)
         box_width = box_x2 - box_x1
-        distance = estimate_distance(know_widht, FOCAL_LENGTH, box_width)
+        distance = estimate_distance(known_width, FOCAL_LENGTH, box_width)
         distances.append(distance)
 
         # Draw a filled rectangle as background for the text
         text = f"Dist: {distance:.2f}m"
-        (text_width, text_height), baseline = cv2.getTextSize(
-            text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2
-        )
-        cv2.rectangle(
-            frame,
-            (int(box_x1), int(box_y1) - text_height - 10),
-            (int(box_x1) + text_width, int(box_y1)),
-            (0, 0, 0),
-            -1,
-        )
+        (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        cv2.rectangle(frame, (int(box_x1), int(box_y1) - text_height - 10), (int(box_x1) + text_width, int(box_y1)), (0, 0, 0), -1)
 
         # Put the distance text on the frame
-        cv2.putText(
-            frame,
-            text,
-            (int(box_x1), int(box_y1) - 5),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (255, 255, 255),
-            2,
-        )
+        cv2.putText(frame, text, (int(box_x1), int(box_y1) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
         # Draw tracker ID for class 1 (platform)
         if detections.class_id[i] == 1 and i < len(detections.tracker_id):
             platform_id = str(detections.tracker_id[i])
             current_platform_ids.add(platform_id)
             current_count = len(box_detections)
-            type = (
-                plataform_data[platform_id]["type"]
-                if platform_id in plataform_data
-                else ""
-            )
+            type = plataform_data[platform_id]["type"] if platform_id in plataform_data else ""
 
             # Update platform data and save type
             if platform_id in plataform_data:
@@ -148,62 +101,31 @@ def process_detections(
             # Estimate the number of boxes on the platform
             if distances:
                 if isinstance(plataform_data[platform_id], dict):
-                    box_count = estimate_box_count(
-                        distances,
-                        box_depth,
-                        platform_depth,
-                        plataform_data[platform_id]["visible_boxes"],
-                    )
+                    closest_distance = min(distances)
+                    average_box_depth = sum(distances) / len(distances)
+                    box_count = estimate_box_count(distances, box_depth, platform_depth, plataform_data[platform_id]["visible_boxes"])
+                    
+                    if len(distances) > 1 and max(distances) > closest_distance + average_box_depth:
+                        box_count -= 1
+
                     plataform_data[platform_id]["total_boxes"] = box_count
-                    draw_text(
-                        frame,
-                        f"Estimated boxes: {box_count}",
-                        (10, 120),
-                        font_scale=1,
-                        color=(0, 255, 0),
-                    )
+                    draw_text(frame, f"Estimated boxes: {box_count}", (10, 120), font_scale=1, color=(0, 255, 0))
 
             # Get item description based on type only if platform_id has changed
-            if (
-                last_platform_id != platform_id
-                and platform_id not in processed_platforms
-            ):
-                update_platform(
-                    platform_id_text,
-                    plataform_data[platform_id]["total_boxes"],
-                    plataform_data[platform_id]["type"],
-                )
+            if last_platform_id != platform_id and platform_id not in processed_platforms:
+                update_platform(platform_id_text, plataform_data[platform_id]["total_boxes"], plataform_data[platform_id]["type"])
                 last_platform_id = platform_id
                 processed_platforms.add(platform_id)
 
-                update_robot(
-                    {
-                        "location": platform_id_text,
-                    }
-                )
+                update_robot({"location": platform_id_text})
 
-            draw_text(
-                frame,
-                f"ID: {detections.tracker_id[i]}",
-                (int(box_x1), int(box_y1) - text_height - 30),
-                font_scale=0.7,
-            )
+            draw_text(frame, f"ID: {detections.tracker_id[i]}", (int(box_x1), int(box_y1) - text_height - 30), font_scale=0.7)
 
     # Save JSON when a platform goes out of the screen
     if last_platform_id and last_platform_id not in current_platform_ids:
         save_data_to_json(plataform_data, output_json)
-        update_platform(
-            plataform_data[last_platform_id]["platform_id"],
-            plataform_data[last_platform_id]["total_boxes"],
-            plataform_data[last_platform_id]["type"],
-        )
-        update_robot(
-            {
-                "location": plataform_data[last_platform_id]["platform_id"],
-            }
-        )
-        last_platform_id = (
-            None  # Reset last_platform_id to indicate the platform has left the screen
-        )
+        update_platform(plataform_data[last_platform_id]["platform_id"], plataform_data[last_platform_id]["total_boxes"], plataform_data[last_platform_id]["type"])
+        update_robot({"location": plataform_data[last_platform_id]["platform_id"]})
+        last_platform_id = None  # Reset last_platform_id to indicate the platform has left the screen
 
     return last_platform_id, processed_platforms
