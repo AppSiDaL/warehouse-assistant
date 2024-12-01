920a84850ebc1970df885de4137eb57b4e89bac6
AppSiDaL <gilielcrack343@gmail.com>
2024-12-01 15:57:22 -0600

    ,

--- robot/utils/serial.py
+++ robot/utils/serial.py
@@ -1,6 +1,6 @@
 import serial
+import serial
 import subprocess
-import time
 
 def get_serial_port():
     try:
@@ -30,17 +30,11 @@ try:
     ser = serial.Serial('/dev/ttyACM0', 9600)  # Ajusta el puerto y la velocidad según sea necesario
 except:
     print("No se pudo conectar al puerto")
+    
 
 def send_command(command):
-    print("moving", command)
-    if ser and command in ['forward', 'backward', 'left', 'right', 'stop', 'light_front', 'shut_front']:
-        try:
-            ser.flushInput()
-            ser.flushOutput()
-            ser.write(command.encode())
-            ser.flush()  # Asegúrate de que los datos se envíen inmediatamente
-            return {"status": "success", "command": command}
-        except Exception as e:
-            print(f"Error al enviar el comando: {e}")
-            return {"status": "error", "message": str(e)}
-    return {"status": "error", "message": "Invalid command or serial port not available"}
+    print("moving",command)
+    if command in ['forward', 'backward', 'left', 'right', 'stop']:
+        ser.write(command.encode())
+        return {"status": "success", "command": command}
+    return {"status": "error", "message": "Invalid command"}
