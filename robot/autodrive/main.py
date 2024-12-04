904c9bdab027512f7c3698e77af57c9e39939a55
AppSiDaL <gilielcrack343@gmail.com>
2024-12-04 00:36:38 -0600

    .

--- robot/autodrive/main.py
+++ robot/autodrive/main.py
@@ -10,8 +10,22 @@ from hailo_rpi_common import (
     get_numpy_from_buffer,
     app_callback_class,
 )
-from utils.serial import send_command
+
 from detection_pipeline import GStreamerDetectionApp
+
+import serial
+
+try:
+    ser = serial.Serial('/dev/ttyACM0', 9600)  # Ajusta el puerto y la velocidad según sea necesario
+except:
+    print("No se pudo conectar al puerto")
+    
+def send_command(command):
+    print("moving",command)
+    if command in ['forward', 'backward', 'left', 'right', 'stop']:
+        ser.write(command.encode())
+        return {"status": "success", "command": command}
+    return {"status": "error", "message": "Invalid command"}
 
 # -----------------------------------------------------------------------------------------------
 # User-defined class to be used in the callback function
