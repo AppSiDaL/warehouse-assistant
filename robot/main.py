4bdc1f0d1cd09b6070c46ea08d57aaf1985c32d4
AppSiDaL <gilielcrack343@gmail.com>
2024-12-03 23:35:12 -0600

    .

--- robot/main.py
+++ robot/main.py
@@ -15,8 +15,8 @@ app.add_middleware(
 )
 
 # Open the two cameras
-camera1 = cv2.VideoCapture(1)
-camera2 = cv2.VideoCapture(0)
+camera1 = cv2.VideoCapture(3)
+camera2 = cv2.VideoCapture(2)
 
 # Allow the cameras to warmup
 time.sleep(0.1)
